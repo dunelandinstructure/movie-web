c75119b385d6e71679f784f47e428bdb68269f17
Iceman0483 <151552261+Iceman0483@users.noreply.github.com>
2023-11-27 19:26:48 +0000

    MOVIE!

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://simple-proxy.iceman3.workers.dev/",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNmFiYWVkYmVlOWU4NTUxZTUyMDk1YzA3NWM1ZDY4YSIsInN1YiI6IjY1MmYwZjAwZWE4NGM3MDBhZWY0ODg5NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.Wm7r96v2bo2UxuEoU5aWJFckJEhrLlx_VSCKoGrP1Uw"
+};
